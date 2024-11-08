438b7fd9a41434f437ecb8b54131992e01ffc561
dstruminska <dstr0007@uni.sydney.edu.au>
2024-11-08 20:48:39 +1100

    Add Extra Elements
    Add elements to the artwork: palm leaves and flower stalks. Update Title.

--- sketch.js
+++ sketch.js
@@ -1,34 +1,106 @@
+
 const colourPalette = ['#E54379', '#CB010B', '#782221', 'purple'];
 const colorKeys = ["flower", "leaves", "endLeaves", "endLeavesStroke"];
 let circles = [];
 let dots = [];
 let endSphereSize,endSphereStroke;
 
-let centerSphereSize = 30; 
+
+let song;
+let Palms = [];
+let numPalms = 20;
+let centerSphereSize = 30;
+
+class Palm {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color; 
+    this.rotation = random(90,270); 
+    this.offset = 0
+  }
+
+  draw() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.rotation); 
+    
+    this.drawPalm();
+    pop();
+  }
+
+  drawPalm() { 
+    let leafCount = 8; // number of leaves per group
+    let angle = 60 / 3; // rotation angle per leaf
+    let leafLength = 90; // palm leaf length
+
+    for (let i = 0; i < leafCount; i++) {
+      this.drawLeaves(angle, leafLength); // pass leaf length to drawing function
+    }
+    // draw the middle of the Palm
+    fill(this.color);
+    noStroke();
+    circle(0, 0, 30);
+  }
+
+  drawLeaves(angle, leafLength) { 
+    let segments = 40; 
+    let px, py;
+
+    // attributes of the leaf curve
+    strokeWeight(5); 
+    stroke(this.color);
+
+    rotate(angle); // rotate leaf to respective angle
+    noFill();
+
+    let xoff = this.offset; //define offset for swaying motion
+    this.offset += 0.0008; //define offset value (smaller value for smoother motion)
+
+    //draw the palm leaf as a curved line:
+    beginShape();
+    for (let i = 0; i < segments; i++) {
+      px = map(i , 0, segments , 0 , leafLength); // define x position of the end of the leaf curve
+      py = sin(i * 10) * (map(noise(xoff), 0, 1, -10, 10)) ; // apply Perlin noise to leaf curve to create a swaying motion
+      vertex(px, py);
+    }
+    endShape();
+
+    this.drawEndLeaf(px, py); 
+  }
+
+
+  drawEndLeaf(px, py) {
+    fill(this.color);
+    strokeWeight(5);
+    stroke(this.color);
+    circle(px, py, 15);
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 
-  initializeElements();
+  initializeElements(); 
 }
 
 function initializeElements() {
 
   const gridSize = windowWidth / 5; 
-  const rows = ceil(windowHeight / gridSize); 
+  const rows = ceil((windowHeight-(windowHeight/5)) / gridSize); 
   const cols = ceil(windowWidth / gridSize);
 
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
-      // Randomize position within the grid cell, with a small random offset
+
       let x = col * gridSize + random(gridSize * 0.2, gridSize * 0.8);
       let y = row * gridSize + random(gridSize * 0.2, gridSize * 0.8);
       let r = random(50, 100);
       let leafCount = Math.floor(random(8,15));
       IndividualRotation = random(120);
-
-      //check if this circle overlaps with any previous circle in circles - Group Code based on 9.8: Random Circles with No Overlap - p5.js Tutorial from The Coding Train (explained in detail in Group Documentation)
+    
+      //check if this circle overlaps with any previous circle in circles
       let overlapping = false;
       for (let other of circles) {
         let d = dist(x, y, other.x, other.y);
@@ -46,9 +118,14 @@ function initializeElements() {
       }
     }
   }
+    for (let i = 0; i < numPalms; i++) {
+      let palmColor = random(colourPalette);
+      let palm = new Palm(random(width), height, palmColor)
+      Palms.push(palm);
+    }
 }
 
-// Util functions:
+// Util functions
 
 function randomColor() {
   return colourPalette[floor(random(colourPalette.length))];
@@ -64,32 +141,30 @@ function createRandomDotsAttributes() {
   };
 }
 
-
 function drawFlower(x, y, leafCount, leafLength, colors, IndividualRotation) {
   push();
-  translate(x, y);
+  translate(x, y); 
   rotate(IndividualRotation) 
-
   endSphereStroke = endSphereSize/3; 
 
+
   if (frameCount < 400){
-  endSphereSize = centerSphereSize/6 * frameCount/300;
+  endSphereSize = centerSphereSize/6 * frameCount/300; 
   }
 
   if (frameCount >= 400 && frameCount < 1400){
-    endSphereSize = centerSphereSize/6 * (600/400+(1+frameCount-400)/200) 
+    endSphereSize = centerSphereSize/6 * (600/400+(1+frameCount-400)/200)
     }
 
   if (frameCount < 360){
-  angleStep = frameCount / leafCount;
+  angleStep = frameCount / leafCount; 
   }
   if (frameCount >= 360){
     angleStep = 360 / leafCount; 
   }
 
-
   for (let i = 0; i < leafCount; i++) {
-    drawLeaves(angleStep, leafLength,colors); 
+    drawLeaves(angleStep, leafLength,colors);
   }
 
   fill(color(colors.flower));
@@ -104,9 +179,9 @@ function drawLeaves(angleStep, leafLength,colors) {
 
   let segments = 15
   let targetSegments = 5 
-  segments = lerp(segments, targetSegments, 0.4)
+  segments = lerp(segments, targetSegments, 0.4) 
 
-  let px, py; 
+  let px, py;
 
   strokeWeight(5); 
   stroke(color(colors.leaves));
@@ -137,7 +212,7 @@ function drawLeaves(angleStep, leafLength,colors) {
       py = sin(i * (15 + (frameCount-600)*0.3)/(600/120)) * 50;
     }
     if (frameCount>=1000){
-      py = sin(i * (15 + (1000-600)*0.3)/(600/120)) * 50;
+      py = sin(i * (15 + (1000-600)*0.3)/(600/120)) * 50; 
     }
     if (frameCount>1400){
       py = (sin(i * (15 + (1000-600)*0.3)/(frameCount-1400)) * 30)
@@ -147,7 +222,6 @@ function drawLeaves(angleStep, leafLength,colors) {
   }
   endShape();
 
- 
   if (frameCount < 1400){
   drawEndLeaf(px, py,colors); 
   }
@@ -157,37 +231,33 @@ function drawLeaves(angleStep, leafLength,colors) {
   }
 }
 
-
 function drawEndLeaf(x, y,colors) {
-
+  // define attributes
   fill(color(colors.endLeaves));
   strokeWeight(endSphereStroke);
   stroke(color(colors.endLeavesStroke));
-
+  // draw small circle
   circle(x, y, endSphereSize);
 }
-
 
 function drawDots() {
   noStroke();
   for (let dot of dots) {
     fill(dot.chosenColor);
     
-    
     dot.x += map(noise(dot.noiseOffset), 0, 1, -2, 2);
     dot.y += map(noise(dot.noiseOffset + 4), 0, 1, -2, 2);
     dot.noiseOffset += 0.01; 
-    
+
     if (frameCount < 1400){
     circle(dot.x, dot.y, dot.size * 0.5);
     }
-    
     if (frameCount >= 1400){
       circle(dot.x, dot.y, dot.size * 0.5 * (100/((frameCount-1400)))); 
     }
-
+ 
     if (dot.x < 0 || width < dot.x || dot.y < 0 || height < dot.y){
-      dot = createRandomDotsAttributes(); 
+      dot = createRandomDotsAttributes();
     }
   }
 
@@ -198,18 +268,35 @@ function drawDots() {
   }
 }
 
+// Main drawing function
 function draw() {
   background(251, 176, 59); 
-  drawDots(); 
+  drawDots();
   
- 
+  
   for (let i = 0; i < circles.length; i++) {
+
+  
+    stroke('purple');
+    strokeWeight(3);
+    noFill();
+
+    let curveX = (circles[i].x - 10, circles[i].x + 15);  // point x curve
+    let curveY = (circles[i].y, height);  // point y curve
+
+    // draw the Bezier curve from the flower center to the bottom of the canvas
+    bezier(circles[i].x, circles[i].y, curveX, curveY, curveX, height, circles[i].x, height);
+
     drawFlower(circles[i].x, circles[i].y, circles[i].leafCount, circles[i].r, circles[i].colors, circles[i].IndividualRotation); 
+  }
+
+  for (let i = 0; i < Palms.length; i++) {
+    Palms[i].draw();
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  initializeElements(); 
+  initializeElements(); // reinitialize elements for new window size
   endSphereStroke = min(windowWidth, windowHeight) / 250; 
 }
